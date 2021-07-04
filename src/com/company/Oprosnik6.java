16abcf86c5b85cfa9daedc1f4c343a3f2b4ac665
unknown <asd@mail.ru>
2021-07-05 02:04:40 +0300

    commit_task1

---
+++ src/com/company/Oprosnik6.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Oprosnik6 {
+    public static void collectionMap(){
+        Map<Integer, String> TheMap = new HashMap<>();
+        TheMap.put(1,"asd");
+        TheMap.put(2,"dsa");
+        TheMap.put(3,"tyu");
+        TheMap.put(4,"abc");
+        TheMap.put(5,"oiu");
+        TheMap.put(6,"abc");
+        TheMap.put(5,"pzy");
+
+        TheMap.size();
+
+        for(Map.Entry<Integer, String> entry: TheMap.entrySet()) {
+            // get key
+            Integer key = entry.getKey();
+            if (entry.getValue() == "abc")  System.out.println(key);
+            // get value
+        }
+    }
+
+    public static void main(String[] args) {
+        collectionMap();
+    }
+
+
+}
